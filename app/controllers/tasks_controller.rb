f091661c1e93c74554268ffb80a5ca54c03b2352
karim issa <issa61139@gmail.com>
2024-04-09 20:53:07 -0400

    more refacturing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,8 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
-  end
-
-  def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -15,32 +12,30 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path
+      redirect_to tasks_path, notice: 'Task was successfully created.'
     else
       render :new
     end
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
-
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
-      redirect_to task_path(@task)
+      redirect_to task_path(@task), notice: 'Task was successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, notice: 'Task was successfully deleted.'
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
